a4eb67378fbf395f1f58f8e0fc1f91968f413510
Hussein Sbeiti <Hussein.sbeiti.wb@gmail.com>
2025-10-24 16:39:59 -0400

    Add Tawk.to live chat widget for instant customer support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -144,6 +144,21 @@ export default function RootLayout({
             </Script>
           </>
         )}
+
+        {/* Tawk.to Live Chat */}
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/68fbc307482c1e1953b84da1/1j8bn3loc';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
         
         {children}
         <ExitIntentPopup />
